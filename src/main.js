286c8851c48c09bfa759cbacbc17fd76b67e08b0
helamanl0424 <helamanlee@gmail.com>
2021-11-05 23:59:50 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import pokemon from './pokemon-data.js'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+let data = {
+  pokemons: pokemon,
+  cart: []
+}
 
 new Vue({
   router,
-  render: (h) => h(App),
-}).$mount("#app");
+  data,
+  render: h => h(App)
+}).$mount('#app')
