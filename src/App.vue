8cf9109cfdc8afb0396b5288c0a08408b959d11b
helamanl0424 <helamanlee@gmail.com>
2021-11-05 23:59:29 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,103 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+<div id="app">
+  <div id="menu">
+    <div id="brand">
+      <router-link to="/">
+        <img src="/images/PokemonLogo.png">
+      </router-link>
     </div>
-    <router-view />
+    <div id="side">
+      <router-link to="/List">
+        <div class="menu-item list">
+          <img src="/images/folder.jpeg">
+          <p>List</p>
+        </div>
+      </router-link>
+      <router-link to="/browse">
+        <div class="menu-item browse">
+          <img src="/images/category.png">
+          <p>Category</p>
+        </div>
+      </router-link>
+      <router-link to="/cart">
+        <div class="menu-item">
+          <img src="/images/ball.png">
+          <p>{{count}} Gotcha</p>
+        </div>
+      </router-link>
+    </div>
   </div>
+  <router-view />
+</div>
 </template>
 
+<script>
+export default {
+  name: 'Cart',
+  computed: {
+    count() {
+      return this.$root.$data.cart.length;
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 50px 100px;
+
+}
+
+#menu {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 40px;
+  grid-template-areas: "none brand side";
+  margin-bottom: 50px;
+}
+
+#menu a {
+  color: #B84901;
+}
+
+#brand {
+  grid-area: brand;
+  display: flex;
+  justify-content: center;
+}
+
+#brand img {
+  height: 200px;
+}
+
+#side {
+  grid-area: side;
+  display: flex;
+  justify-content: space-around;
+}
+
+#side img {
+  width: 50px;
+}
+
+.menu-item {
+  display: flex;
+  flex-direction: column;
+}
+
+.menu-item p {
+  margin: 0px;
+}
+
+p {
   text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+/* .browse {
+  margin-left: 10px;
+} */
 </style>
